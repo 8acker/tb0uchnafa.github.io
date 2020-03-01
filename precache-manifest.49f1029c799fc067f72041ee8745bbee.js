ad424eda757af04b04629a41e8c63fa89dc42764
Thami Bouchnafa <thami.bouchnafa@gmail.com>
2020-03-01 14:55:31 +0100

    Updates

---
+++ precache-manifest.49f1029c799fc067f72041ee8745bbee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbe55471edde3720157b8d62b0efcb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f747cfd1e29ad10b22a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eb9c013ce9661c3aaeaf",
+    "url": "/static/js/2.4be7cfc0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4be7cfc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f747cfd1e29ad10b22a",
+    "url": "/static/js/main.f843e2ba.chunk.js"
+  },
+  {
+    "revision": "29c50b6de12daab70dc0",
+    "url": "/static/js/runtime-main.336826c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
