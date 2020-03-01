c2f599862e953b0a1e143c8f2ccf8882aa913bc4
Thami Bouchnafa <thami.bouchnafa@gmail.com>
2020-03-01 09:26:21 +0100

    Updates

---
+++ precache-manifest.f98bbd70abadc9ff77879afbaea3162e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f599b1259f25ac3ce11993b2d80926a",
+    "url": "/web-app/index.html"
+  },
+  {
+    "revision": "6a5187ccb869808f03c4",
+    "url": "/web-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eb9c013ce9661c3aaeaf",
+    "url": "/web-app/static/js/2.4be7cfc0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/web-app/static/js/2.4be7cfc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a5187ccb869808f03c4",
+    "url": "/web-app/static/js/main.e5d446bf.chunk.js"
+  },
+  {
+    "revision": "70ce03cd20f8980ded7c",
+    "url": "/web-app/static/js/runtime-main.7e6367c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
